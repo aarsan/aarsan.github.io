3f1731350b0e9e898372c379dc5087e4a76c869f
Ahmet Arsan <ahmet.arsan@zsassociates.com>
2015-03-27 14:44:53 -0500

    adding appfactory

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 var app = angular.module('site', ['ui.bootstrap', 'ngAria']);
+
+
+app.factory('Backend', ['$http',
+    function($http) {
+        var get = function(url) {
+            return function() {
+                return $http.get(url).then(function(resp) {
+                    return resp.data;
+                });
+            }
+        };
+
+        return {
+            ahmet: get('data/mydata.json')
+        }
+    }
+])
