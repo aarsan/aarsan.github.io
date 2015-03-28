eba284bf76e6c0012fbadf578ee99906f1a9ffe5
Ahmet Arsan <aarsan@progger.com>
2015-03-28 16:49:02 -0500

    angular fun

---
+++ js/app.js
@@ -0,0 +1,22 @@
+angular.module('app', []);
+
+
+// myinfo = [./data/mydata.json]
+
+
+
+// app.factory('Backend', ['$http',
+//     function($http) {
+//         var get = function(url) {
+//             return function() {
+//                 return $http.get(url).then(function(resp) {
+//                     return resp.data;
+//                 });
+//             }
+//         };
+
+//         return {
+//             ahmet: get('data/mydata.json')
+//         }
+//     }
+// ])
